84ed81578d120cad33fdbde69c40b3fc7a4cfd67
RobertoPrete <robertoprete2001@gmail.com>
2025-06-27 15:29:49 +0200

    top vendite senza filtri (TopVendite1)

--- model/model.py
+++ model/model.py
@@ -21,6 +21,14 @@ class Model:
         self.listaRetailers = DAO.getRetailers()
 
     def top_vendite(self, anno, brand, retailer):
+        """
+        Questo metodo del modello permette di recuperare dal database il top delle vendite (massimo 5) in base ai filtri selezionati nei dropdown di anno, brand e retailer.
+        Recupera dati diversi in base ai filtri selezionati e non. Per questo differenziamo le top vendite nel seguente modo:
+        TopVendite1: le top vendite date nel caso in cui non viene selezionato nessun filtro tra anno, brand e retailer (o viene selezionato in tutti i casi "Nessune filtro")
+        TopVendite2: le top vendite date nel caso in cui viene selezionato solo il filtro dell'anno
+        TopVendite3: le top vendite date nel caso in cui vengono selezionati tutti i filtri (anno, brand e retailer)
+
+        """
         if (anno is None or anno == "" or anno == "Nessun filtro") and (brand is None or brand == "" or brand == "Nessun filtro") and (retailer is None or retailer == "" or retailer == "Nessun filtro"):
             topVendite = DAO.getTopVendite1()
         return topVendite
